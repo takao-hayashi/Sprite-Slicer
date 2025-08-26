b15ef2fc80a5798fa9cbc519215847bcfa7e4b25
Takao Hayashi <aineuros023@proton.me>
2025-08-26 14:02:45 +0900

    Add files via upload

---
+++ Public/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'sprite-slicer-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/src/main.ts',
+  '/src/style.css',
+  '/manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
